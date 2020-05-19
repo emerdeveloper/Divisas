23c8d8924700a4811bc6256792740289d801f27e
Emerson Gonzalez <emersonjavid97@gmail.com>
2020-05-18 21:53:47 -0500

    add base exchange Bitcoin

---
+++ Divisas/Controller/BitCoinViewController.swift
@@ -0,0 +1,53 @@
+//
+//  BitCoinViewController.swift
+//  Divisas
+//
+//  Created by Emerson Javid Gonzalez Morales on 18/05/20.
+//  Copyright © 2020 Emerson Javid Gonzalez Morales. All rights reserved.
+//
+
+import UIKit
+
+class BitCoinViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var labelBitCoin: UILabel!
+    @IBOutlet weak var labelCurrency: UILabel!
+    @IBOutlet weak var pickerCurrency: UIPickerView!
+    
+    let coinManager = CoinManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        pickerCurrency.dataSource = self
+        pickerCurrency.delegate = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        labelCurrency.text = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: labelCurrency.text!)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Divisas/Model/CoinManager.swift
@@ -0,0 +1,38 @@
+//
+//  CoinManager.swift
+//  Divisas
+//
+//  Created by Emerson Javid Gonzalez Morales on 18/05/20.
+//  Copyright © 2020 Emerson Javid Gonzalez Morales. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "?apikey=346105E3-2E9C-455D-B49E-5721EE19FE72"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        let url = "\(baseURL)/\(currency)\(apiKey)"
+        performRequest(with: url)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                }
+                
+                if let safeData = data {
+                    print(safeData)
+                }
+            }
+            
+            task.resume()
+        }
+    }
+}
